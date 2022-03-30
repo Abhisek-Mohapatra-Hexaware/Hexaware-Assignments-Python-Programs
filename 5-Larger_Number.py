97c79db7a555fcbe6ccf68fcc5fa165aea7f8eaf
Abhisek Mohapatra <48471558+Alpha-Stark@users.noreply.github.com>
2022-03-31 01:41:02 +0530

    Update 5-Larger_Number.py

--- 5-Larger_Number.py
+++ 5-Larger_Number.py
@@ -1,5 +1,5 @@
-num1=input("Enter 1st number: ")
-num2=input("Enter 2nd number: ")
+num1=int(input("Enter 1st number: "))
+num2=int(input("Enter 2nd number: "))
 if(num1>num2):
     print("num1 is larger ",num1)
 else:
