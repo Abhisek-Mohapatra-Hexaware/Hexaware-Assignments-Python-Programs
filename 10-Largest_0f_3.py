d60bd322694e79268c3138cc1feec12f889f0ce0
Abhisek Mohapatra <48471558+Alpha-Stark@users.noreply.github.com>
2022-03-31 01:37:00 +0530

    Update 10-Largest_0f_3.py

--- 10-Largest_0f_3.py
+++ 10-Largest_0f_3.py
@@ -1,6 +1,6 @@
-num1=input("Enter number1: ")
-num2=input("Enter number2: ")
-num3=input("Enter number3: ")
+num1=int(input("Enter number1: "))
+num2=int(input("Enter number2: "))
+num3=int(input("Enter number3: "))
 if(num1>num2 and num1>num3):
   largest=num1
 elif(num2>num3 and num2>num1):
